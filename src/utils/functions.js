a5e327de6818b8c90920ebcfea38865af475e2c3
Benavente <matias.benavente@anima.edu.uy>
2022-09-30 19:01:17 -0300

    geocode basic integration

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,34 +1,54 @@
+import { getCoordinatesFromAddress } from "../api/geocode";
+
 export const isMobile = () => {
-  return window.innerWidth <= 960 ? true : false
-}
+  return window.innerWidth <= 960 ? true : false;
+};
 
 export const parseCentreFormValues = (values) => {
-  const cSchParsedValues = []
-  const careerParsedValues = []
-  Object.entries(values).forEach(([key ,value]) => {
-    if (key === 'centreSchedule') {
-      Object.entries(value).forEach(([,{ value }]) => {
-        cSchParsedValues.push(value)
-      })
+  const cSchParsedValues = [];
+  const careerParsedValues = [];
+  let centreAdressStreet = "";
+  let centreAdressNumber = 0;
+  let centreAddress = "";
+  let latitude = 0;
+  let longitude = 0;
+  Object.entries(values).forEach(([key, value]) => {
+    if (key === "centreSchedule") {
+      Object.entries(value).forEach(([, { value }]) => {
+        cSchParsedValues.push(value);
+      });
     }
-    if (key === 'careers') {
-      Object.entries(value).forEach(([,value]) => {
-        careerParsedValues.push(value)
-      })
+    if (key === "careers") {
+      Object.entries(value).forEach(([, value]) => {
+        careerParsedValues.push(value);
+      });
     }
-  })
+
+    if (key === "adressStreet") {
+      centreAdressStreet = value;
+    }
+    if (key === "addressNumber") {
+      centreAdressNumber = value;
+    }
+    centreAddress = centreAdressStreet + " " + centreAdressNumber;
+    const { latitudeA, longitudeA } = getCoordinatesFromAddress(centreAddress);
+    latitude = latitudeA;
+    longitude = longitudeA;
+  });
   const data = {
     ...values,
+    latitude: latitude,
+    longitude: longitude,
     centreSchedule: cSchParsedValues,
-    careers: careerParsedValues
-  }
-  return data
-}
+    careers: careerParsedValues,
+  };
+  return data;
+};
 
 export const checkIfExists = (data, item) => {
-   if (data.includes(item)){
-      return true
-   }else {
-      return false
-   }
-}
+  if (data.includes(item)) {
+    return true;
+  } else {
+    return false;
+  }
+};
